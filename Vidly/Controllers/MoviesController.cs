b30312c74638a64cfa9db1c57eb6b7d34d6a1033
tolmivan <ivan@DESKTOP-MDB3PVK>
2016-11-04 21:49:02 +1100

    EF Exercise 3

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -11,51 +12,35 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
 
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(c=>c.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            var movies = new List<Movie>
-            {
-                new Movie() {Id = 1, Name = "Sherk"},
-                new Movie() {Id = 2, Name = "Wall-e"}
-            };
 
-            return movies;
+            var movie = _context.Movies.Include(m => m.Genre).FirstOrDefault(m => m.Id == id);
+
+            return View(movie);
         }
 
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() {Name = "Shrek!"};
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-             
-            return View(viewModel);
-            
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
 
     }
 }
