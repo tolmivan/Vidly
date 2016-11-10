c07216e9f8616a78676a3a7b923b37ecdf54b2e9
tolmivan <ivan@DESKTOP-MDB3PVK>
2016-11-10 11:20:14 +1100

    Section 4 Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -41,6 +41,54 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres;
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
 
+            return View("MovieForm", viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
+                return new HttpNotFoundResult();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -14,11 +14,17 @@ namespace Vidly.Models
         [StringLength(50)]
         public string Name { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        public DateTime DateAdded { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
 
     }
