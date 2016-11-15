fa19744e6e3396b7e50befcc3b5177a1d4a4ee7c
tolmivan <ivan@DESKTOP-MDB3PVK>
2016-11-15 13:11:34 +1100

    refactor account view models

---
+++ Vidly/ViewModels/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Vidly.ViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ Vidly/ViewModels/ForgotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
